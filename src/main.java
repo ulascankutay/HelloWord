1911d786660128b37746ac1b81acde76c20ee555
ulascankutay <92178308+ulascankutay@users.noreply.github.com>
2021-10-08 20:51:18 +0300

    CU bulma

--- src/main.java
+++ src/main.java
@@ -1,9 +1,10 @@
 public class main {
     public static void main(String[] args) {
        hello();
-    }    
+    }
      public static void hello (){
          System.out.println("hello word");
+         System.out.println("CUUUUUUUUU");
      }
 }
 
