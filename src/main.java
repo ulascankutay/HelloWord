68c6461f9869476bfaa6711c959a77780c5639c8
ulascankutay <92178308+ulascankutay@users.noreply.github.com>
2021-10-08 20:37:35 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+       hello();
+    }    
+     public static void hello (){
+         System.out.println("hello word");
+     }
+}
+
